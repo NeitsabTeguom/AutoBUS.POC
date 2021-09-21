64577796864827896f7d7727e0f8c8579fd37e0e
Bastien MOUGET <bm@neitsab.fr>
2021-09-21 23:01:33 +0200

    Messages V1

---
+++ AutoBUS.Common/Broker/Broker.Messages.V1.cs
@@ -0,0 +1,27 @@
+﻿namespace AutoBUS.Messages.V1
+{
+    public static class Receive
+    {
+        /// <summary>
+        /// Login
+        /// </summary>
+        /// <param name="broker"></param>
+        /// <param name="SocketId"></param>
+        /// <param name="frame"></param>
+        public static void Login(Broker broker, long SocketId, Broker.Frame frame)
+        {
+        }
+    }
+
+    public static class Send
+    {
+        /// <summary>
+        /// Login
+        /// </summary>
+        /// <param name="broker"></param>
+        /// <param name="SocketId"></param>
+        public static void Login(Broker broker, long SocketId)
+        {
+        }
+    }
+}
